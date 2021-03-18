31f546f629e6719d9fce7523fa6bfde4c6ac89ee
Fiona Soetrisno <fiona.soetrisno@gmail.com>
2021-03-18 04:35:30 -0700

    init

---
+++ Assets/Scripts/VRManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR;
+
+public class VRManager : MonoBehaviour
+{
+    public bool showVR = false;
+    public float playerScale = 5f;
+    public Transform player;
+
+    private void Update()
+    {
+        // TODO: Move somewhere else
+
+        UnityEngine.XR.XRSettings.showDeviceView = true;
+        player.localScale = new Vector3(playerScale, playerScale, playerScale);
+
+    }
+}
+
